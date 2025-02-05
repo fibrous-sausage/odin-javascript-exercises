5728ba0b35a41efa4bd79c1c09b5ae13f18276ff
fibrous <65938238+fibrous-sausage@users.noreply.github.com>
2025-02-05 10:02:28 -0500

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    return array.filter(element => toRemove.indexOf(element) === -1);
 };
 
 // Do not edit below this line
