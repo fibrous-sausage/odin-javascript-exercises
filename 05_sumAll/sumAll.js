2ac1969aa975566cc9c408823c0112dcecaef6fd
fibrous <65938238+fibrous-sausage@users.noreply.github.com>
2025-02-05 10:12:46 -0500

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(lowerBound, upperBound) {
+    if (typeof(lowerBound) !== "number" || typeof(upperBound) !== "number") {
+        return "ERROR";
+    }
 
+    if (lowerBound < 0 || upperBound < 0) {
+        return "ERROR";
+    }
+
+    if (lowerBound !== Math.floor(lowerBound) || upperBound !== Math.floor(upperBound)) {
+        return "ERROR";
+    }
+
+    if (lowerBound > upperBound) {
+        let temp = upperBound;
+        upperBound = lowerBound;
+        lowerBound = temp;
+    }
+
+    let sum = 0;
+    for (let i = lowerBound; i <= upperBound; ++i) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
