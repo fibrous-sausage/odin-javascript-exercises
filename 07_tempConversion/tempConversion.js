d2277865a36cdbfe97ad175582af047162d4562a
fibrous <65938238+fibrous-sausage@users.noreply.github.com>
2025-02-05 10:25:14 -0500

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const roundFirstDecimal = function(num) {
+  return Math.round(num * 10) / 10;
+}
+
+const convertToCelsius = function(fahrenheit) {
+  return roundFirstDecimal(5/9 * (fahrenheit - 32));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return roundFirstDecimal(celsius * 9/5 + 32);
 };
 
 // Do not edit below this line
