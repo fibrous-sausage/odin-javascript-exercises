f79f4a0c28202fc5de34e8098120a7fac2b8afff
fibrous <65938238+fibrous-sausage@users.noreply.github.com>
2025-02-09 18:30:11 -0500

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = (new Date()).getFullYear();
+    return people.reduce((oldestPerson, person) => {
+        const oldestPersonAge = ("yearOfDeath" in oldestPerson ? oldestPerson.yearOfDeath : currentYear) - oldestPerson.yearOfBirth;
+        const personAge = ("yearOfDeath" in person ? person.yearOfDeath : currentYear) - person.yearOfBirth;
+        return oldestPersonAge > personAge ? oldestPerson : person;
+    }, people.at(0));
 };
 
 // Do not edit below this line
