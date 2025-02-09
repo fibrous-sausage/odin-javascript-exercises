06ab305249e51140a78e3331d1a24ea34f44d1b9
fibrous <65938238+fibrous-sausage@users.noreply.github.com>
2025-02-09 18:20:43 -0500

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n < 0) {
+        return "OOPS"
+    } else if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        const worker = (counter, last, current) => {
+            if (counter === n) {
+                return current;
+            } else {
+                return worker(counter + 1, current, last + current);
+            }
+        }
+        return worker(1, 0, 1);
+    }
 };
 
 // Do not edit below this line
