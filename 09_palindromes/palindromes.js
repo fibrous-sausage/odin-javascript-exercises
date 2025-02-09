369b15efcda9c489268960632f43d81bf344ed91
fibrous <65938238+fibrous-sausage@users.noreply.github.com>
2025-02-09 17:36:43 -0500

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let processedStr = str.toLowerCase()
+                            .split("")
+                            .filter(character => character.match(/^[0-9a-z]+$/));
+    const halfLength = processedStr.length / 2;
+    for (let i = 0; i < halfLength; ++i) {
+        if (processedStr.at(i) !== processedStr.at(processedStr.length - 1 - i)) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
