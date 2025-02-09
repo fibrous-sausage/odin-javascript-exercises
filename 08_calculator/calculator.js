771a2dfcd7b25fa618bb17fb6ecbd5f56be82ef8
fibrous <65938238+fibrous-sausage@users.noreply.github.com>
2025-02-09 17:17:43 -0500

    Complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(args) {
+	return args.reduce((accum, curr) => accum + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(args) {
+  return args.reduce((accum, curr) => accum * curr, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let accum = 1;
+  for (let k = n; k > 0; --k) {
+    accum *= k;
+  }
+  return accum;
 };
 
 // Do not edit below this line
