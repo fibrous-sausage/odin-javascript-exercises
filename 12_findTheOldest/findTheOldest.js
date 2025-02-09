7be7b84378e6175fdf91415921e1de3a99d33687
fibrous <65938238+fibrous-sausage@users.noreply.github.com>
2025-02-09 18:32:39 -0500

    Improve 12 with nullish coalescing

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,8 +1,8 @@
 const findTheOldest = function(people) {
     const currentYear = (new Date()).getFullYear();
     return people.reduce((oldestPerson, person) => {
-        const oldestPersonAge = ("yearOfDeath" in oldestPerson ? oldestPerson.yearOfDeath : currentYear) - oldestPerson.yearOfBirth;
-        const personAge = ("yearOfDeath" in person ? person.yearOfDeath : currentYear) - person.yearOfBirth;
+        const oldestPersonAge = (oldestPerson.yearOfDeath ?? currentYear) - oldestPerson.yearOfBirth;
+        const personAge = (person.yearOfDeath ?? currentYear) - person.yearOfBirth;
         return oldestPersonAge > personAge ? oldestPerson : person;
     }, people.at(0));
 };
