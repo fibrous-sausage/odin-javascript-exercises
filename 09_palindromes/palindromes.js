7ef07dafdac7ab80a9fdc3543c4179501fdfa9ed
fibrous <65938238+fibrous-sausage@users.noreply.github.com>
2025-02-09 17:50:38 -0500

    Clean up 09 based on solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,12 +1,14 @@
 const palindromes = function (str) {
-    let processedStr = str.toLowerCase()
-                            .split("")
-                            .filter(character => character.match(/^[0-9a-z]+$/));
-    const halfLength = processedStr.length / 2;
-    for (let i = 0; i < halfLength; ++i) {
-        if (processedStr.at(i) !== processedStr.at(processedStr.length - 1 - i)) return false;
-    }
-    return true;
+    // Explanation of regex:
+    // ^ means beginning of string, $ means end of string
+    // [] means match each character to a character inside the braces
+    // 0-9 is short for all numbers from 0-9
+    // a-z is short for all lowercase letters from a-z
+    const cleanStr = str.toLowerCase()
+                        .split("")
+                        .filter(character => character.match(/^[0-9a-z]+$/));
+    const reverseStr = cleanStr.toReversed();
+    return cleanStr.join("") === reverseStr.join("");
 };
 
 // Do not edit below this line
